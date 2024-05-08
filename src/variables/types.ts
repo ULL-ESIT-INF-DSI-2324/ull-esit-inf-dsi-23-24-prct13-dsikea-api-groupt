18252a4fcfd49d62a91eff3f932735e26058d964
Guillermo Plaza <alu0101495354@ull.edu.es>
2024-05-08 15:24:07 +0000

    added custom types file for better look of the code

---
+++ src/variables/types.ts
@@ -0,0 +1,3 @@
+export type Genre = "male" | "female" | undefined;
+export type Color = "white" | "black" | "blue" | "red" | "yellow" | "green" | "brown" | "gray" | "other";
+export type Material = "wood" | "metal" | "unpholstery" | "plastic" | "glass" | "stone" | "other";
