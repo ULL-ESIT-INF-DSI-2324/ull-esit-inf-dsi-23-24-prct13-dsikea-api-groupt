954069ba2c345afe4cc7ae6b075a532e17425bcc
Guillermo Plaza <alu0101495354@ull.edu.es>
2024-05-09 18:45:09 +0000

    changing cluster... again

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,7 +1,7 @@
 import { connect } from "mongoose";
 
 try {
-  await connect('mongodb+srv://guillermoplaza:Seguridad23@dsi-groupt-dsikea.czuoep8.mongodb.net/');
+  await connect('mongodb+srv://guillermoplaza:dsi@dsi-groupt-dsikea.czuoep8.mongodb.net/');
   console.log('Connection stablished in https://ull-esit-inf-dsi-23-24-prct13-dsikea-api-3m1d.onrender.com');
 } catch (err) {
   console.log(err);
